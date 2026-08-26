361521406f4e37c692cca6b4c5de9cff71d68ec6
ganes <ganesh.cs.nc@cambridge.edu.in>
2026-08-26 20:30:30 +0530

    first commit

--- TNSIF_TRANING/src/org/tnsif/acc/c2tc/oops/ConstructorOverloading.java
+++ TNSIF_TRANING/src/org/tnsif/acc/c2tc/oops/ConstructorOverloading.java
@@ -1,21 +1,19 @@
 package org.tnsif.acc.c2tc.oops;
-
-
-class Student4
+class Student1
 {
 	String name;
 	int age;
 	
-	Student4()
+	Student1()
 	{
 		System.out.println("Default constructor");
 	}
-	Student4(String name)
+	Student1(String name)
 	{
 		this.name=name;
 		System.out.println("Name "+name);
 	}
-	Student4(String name,int age)
+	Student1(String name,int age)
 	{
 		this.name=name;
 		this.age=age;
@@ -25,13 +23,10 @@ class Student4
 public class ConstructorOverloading {
 
 	public static void main(String[] args) {
-		Student4 student1=new Student4();
-		Student4 student2=new Student4("ganesh");
-		Student4 student3=new Student4("gagan",22);
-		
+		Student1 s1=new Student1();
+		Student1 s3=new Student1("Hema",26);
+		Student1 s2=new Student1("Hema");
 	
-		
-		
 
 	}
 
